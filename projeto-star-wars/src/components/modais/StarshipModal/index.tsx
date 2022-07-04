85871eb99840a204c6e4378f25f230e4ed1de25f
alansmello <alansmello@gmail.com>
2022-07-04 14:28:01 -0300

    mudanças no Modal

--- projeto-star-wars/src/components/modais/StarshipModal/index.tsx
+++ projeto-star-wars/src/components/modais/StarshipModal/index.tsx
@@ -1,28 +1,32 @@
-import React from "react";
-import {Modal, ModalProps, Text} from 'react-native';
-import { starShipList } from "../../../screens/LojaNaves";
+import React, { useState } from "react";
+import { Modal, ModalProps, Text, TouchableOpacity, View } from 'react-native';
+import { Item } from "react-native-paper/lib/typescript/components/List/List";
+import { starship } from "../../../screens/LojaNaves";
 
 interface modalProps extends ModalProps {
-    item: starShipList,
-    modal:boolean,
+    item: starship,
+    modal: boolean,
+    setModal: React.Dispatch<React.SetStateAction<boolean>>,
     preco?: number
 }
 
-export const StarshipModal = (item, modal, preco?)=>{
-    
-    return (
-    <Modal
+export const StarshipModal = ({item, modal, setModal, preco, ...rest}:modalProps) => {
+
+    return <Modal
         animationType="slide"
-        transparent={false}
+        transparent={true}
         visible={modal}
         onRequestClose={() => {
-            false;
+            setModal(false)
         }}
-        //{...rest}
     >
-        <Text style={{color:'white'}}>{item.name}</Text>
+        <TouchableOpacity onPress={() => setModal(false)}>
+            <View style={{ backgroundColor: 'red', height: 200, width: '100%' }}>
+                <Text style={{ color: 'white' }}>{item.name}</Text>
+            </View>
+        </TouchableOpacity>
     </Modal>
 
-    
-    )
+
+
 }
--- projeto-star-wars/src/screens/LojaNaves/index.tsx
+++ projeto-star-wars/src/screens/LojaNaves/index.tsx
@@ -1,98 +1,126 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { View, Text, FlatList, Linking, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, Linking, TouchableOpacity, Modal } from 'react-native';
 import { Botao } from '../../components/Botao';
 import { StarshipModal } from '../../components/modais/StarshipModal';
 import { buscaStarships } from '../../services/api';
 import { styles } from './styles';
 
 
-export interface starShipList{
-    name: string, 
-    model: string, 
-    manufacturer: string, 
-    cost_in_credits: string, 
-    length: string, 
-    max_atmosphering_speed: string, 
-    crew: string, 
-    passengers: string, 
-    cargo_capacity: string, 
-    consumables:string, 
-    hyperdrive_rating: string, 
-    MGLT: string, 
-    starship_class: string, 
-    pilots: [], 
-    films: [], 
-    created: string, 
-    edited: string, 
+export interface starship {
+    name: string,
+    model: string,
+    manufacturer: string,
+    cost_in_credits: string,
+    length: string,
+    max_atmosphering_speed: string,
+    crew: string,
+    passengers: string,
+    cargo_capacity: string,
+    consumables: string,
+    hyperdrive_rating: string,
+    MGLT: string,
+    starship_class: string,
+    pilots: [],
+    films: [],
+    created: string,
+    edited: string,
     url: string
 }
 
-export const LojaNaves = ()=>{
-    
-    const [starShipList, setStarShipList] = useState<starShipList[]>([]);
-    const[page, setPage] = useState<string>("");
+export const LojaNaves = () => {
+
+    const [starShipList, setStarShipList] = useState<starship[]>([]);
+    const [page, setPage] = useState<string>("");
     const [loading, setLoading] = useState<boolean>(true);
     const [modal, setModal] = useState<boolean>(false);
-
-
+    const [starshipItem, setStarshipItem] = useState<starship>(
+        {
+            name: "",
+            model: "",
+            manufacturer: "",
+            cost_in_credits: "",
+            length: "",
+            max_atmosphering_speed: "",
+            crew: "",
+            passengers: "",
+            cargo_capacity: "",
+            consumables: "",
+            hyperdrive_rating: "",
+            MGLT: "",
+            starship_class: "",
+            pilots: [],
+            films: [],
+            created: "",
+            edited: "",
+            url: ""
+        }
+    );
 
     useEffect(() => {
         setLoading(true);
         buscaStarships(page).then((res) => {
-        setStarShipList(res.data.results);
-       
-        }).catch(() => {
-            console.log("Erro")
-        }).finally(() => setLoading(false))    
-    }, [page]);
+            setStarShipList(res.data.results);
 
+        }).catch((err) => {
+            console.log(err)
+        }).finally(() => setLoading(false))
+    }, [page]);
+    
+    
     return (
         <View style={styles.container}>
             <Text style={styles.title}>
                 Loja de Naves
             </Text>
-         
-           
+
+
             {loading ?
                 <Text style={styles.cardTitle}>
                     Carregando...
                 </Text>
                 :
-                <FlatList
-                    data={starShipList}
-                    showsVerticalScrollIndicator={false}
-                    
-                    renderItem={({ item, index }) => (
-                        <>
-                        <View>
-                            <TouchableOpacity
-                            onPress={()=> setModal(true)}
-                            >
-                            <Text style={styles.cardTitle}>Nave: {item.name}</Text>
-                           <Text style={styles.text}>Modelo: {item.model}</Text> 
-                                                       
-                          </TouchableOpacity>
-                           
-                          <StarshipModal item={item} modal={true} />
-                       </View>
-                       
-                       
-                       
-                      </>
-                    )
-                    
-                    }
-                />
-                
+                <>
+                    <FlatList
+                        data={starShipList}
+                        showsVerticalScrollIndicator={false}
+
+                        renderItem={({ item, index }) => (
+
+                            <>
+                                <View>
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            setModal(true)
+                                            setStarshipItem(item)
+                                        }}
+                                    >
+                                        <Text style={styles.cardTitle}>Nave: {item.name}</Text>
+                                        <Text style={styles.text}>Modelo: {item.model}</Text>
+
+                                    </TouchableOpacity>
+
+
+                                </View>
+
+
+
+                            </>
+                        )
+
+                        }
+                    />
+                    <StarshipModal item={starshipItem} modal={modal} setModal={setModal} />
+
+                </>
             }
-            
+
             <View style={styles.buttons}>
-            <Botao titulo={'1'} corFundo={'white'} corTexto={'black'} onPress={()=> setPage("?page=1")}></Botao>
-            <Botao titulo={'2'} corFundo={'white'} corTexto={'black'} onPress={()=> setPage('?page=2')}></Botao>
-            <Botao titulo={'3'} corFundo={'white'} corTexto={'black'} onPress={()=> setPage("?page=3")}></Botao>
-            <Botao titulo={'4'} corFundo={'white'} corTexto={'black'} onPress={()=> setPage('?page=4')}></Botao>
+                <Botao titulo={'1'} corFundo={'white'} corTexto={'black'} onPress={() => setPage("?page=1")}></Botao>
+                <Botao titulo={'2'} corFundo={'white'} corTexto={'black'} onPress={() => setPage('?page=2')}></Botao>
+                <Botao titulo={'3'} corFundo={'white'} corTexto={'black'} onPress={() => setPage("?page=3")}></Botao>
+                <Botao titulo={'4'} corFundo={'white'} corTexto={'black'} onPress={() => setPage('?page=4')}></Botao>
             </View>
-            
+
         </View>
     )
 }
