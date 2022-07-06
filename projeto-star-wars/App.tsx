2dbe675bb5fae78f6166e85d495f8a9d89631183
alansmello <alansmello@gmail.com>
2022-07-06 19:35:46 -0300

    implentado animatedSplash

---
+++ projeto-star-wars/App.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import { Routes } from "./src/routes";
+import AnimatedSplash from "react-native-animated-splash-screen";
+import Logo from './src/assets/images/Hondo_Ohnaka_pirate_symbol.webp';
+
+const App = () => {
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoaded(true);
+        }, 2000)
+    });
+  return(
+    <AnimatedSplash
+                translucent={true}
+                isLoaded={isLoaded}
+                logoImage={Logo}
+                backgroundColor={"#262626"}
+                logoHeight={150}
+                logoWidth={150}
+            >
+        <Routes/>
+  </AnimatedSplash>
+  )
+}
+
+export default App;
