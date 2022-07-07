11c30b577434b102b631e966cd123e43fd882802
Isabel-Bringhenti <belmedeirosb@gmail.com>
2022-07-06 22:02:48 -0300

    styles feitos

--- projeto-star-wars/src/routes/index.tsx
+++ projeto-star-wars/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
@@ -6,13 +7,17 @@ import { LojaNaves } from '../screens/LojaNaves';
 import { Home } from '../screens/Home';
 import { Login } from '../screens/Login';
 
+import HomeIcon from "../assets/icons/home.png";
+import LojaIcon from "../assets/icons/ship.png";
+import CarrinhoIcon from "../assets/icons/credits.png"
+
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 const Feed = () => {
     return(
     <Stack.Navigator>
-                <Tab.Screen name="Home" component={Home} />
+                <Tab.Screen name="teste" component={Home}  />
                 <Stack.Screen name="Login" component={Login} />
             </Stack.Navigator>
     )
@@ -24,10 +29,25 @@ export const Routes = () => {
         <Tab.Navigator
         screenOptions={{
             headerShown: false,
+            tabBarStyle: { backgroundColor: "#000", paddingVertical: 2 },
+            tabBarActiveTintColor: '#fff',
+            tabBarInactiveTintColor: '#a5a9a8',
         }}
         >
-            <Tab.Screen name="Feed" component={Feed} />
-            <Tab.Screen name="LojaNaves" component={LojaNaves} />
+            <Tab.Screen name="Home" component={Feed} options={{tabBarIcon: ({color}) => (
+                            <Image
+                                resizeMode='contain'
+                                style={{width: 30, tintColor: color }}
+                                source={HomeIcon}
+                            />
+                        ),}} />
+            <Tab.Screen name="Loja" component={LojaNaves} options={{tabBarIcon: ({color}) => (
+                            <Image
+                                resizeMode='contain'
+                                style={{width: 30, tintColor: color }}
+                                source={LojaIcon}
+                            />
+                        ),}}/>
         </Tab.Navigator>
     </NavigationContainer>
   );
