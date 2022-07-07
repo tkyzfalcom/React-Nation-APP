b69b530bcbde7f6e341f1e2f2aa9fa1a1866b946
Carlos Eduardo Vasconcelos Pereira <Carlosevpereira8@gmail.com>
2022-07-07 17:59:49 -0300

    Update styles.ts

--- projeto-star-wars/src/screens/Cart/styles.ts
+++ projeto-star-wars/src/screens/Cart/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create ({
     container: {
         flex: 1,
-        backgroundColor: '#121015',
+        backgroundColor: '#181818',
 
     },
     text: {
